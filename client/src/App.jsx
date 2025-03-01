7976fa65ceea4a7ce39f017eb50ce897f7d0cf54
Siyam <bhuiyansiyam294@gmail.com>
2025-03-01 19:58:21 +0600

    CafeteriaManagement Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+//path: client/src/App.jsx
 import BusSchedule from "./components/BusSchedule";
 import CafeteriaMenu from "./components/CafeteriaMenu";
 import Landing from "./components/Landing";
@@ -6,23 +7,28 @@ import ClassSchedule from "./components/ClassSchedule";
 import CampusNavigation from "./components/CampusNavigation";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./components/Homepage";
+import AdminDashboard from "./components/Admin/AdminDashboard";
+import CafeteriaManagement from "./components/Admin/CafeteriaManagement";
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Landing />} />
-
-          <Route path="/BusSchedule" element={<BusSchedule />} />
-          <Route path="/CafeteriaMenu" element={<CafeteriaMenu />} />
-          <Route path="/EventCalendar" element={<EventCalendar />} />
-          <Route path="/ClassSchedule" element={<ClassSchedule />} />
-          <Route path="/CampusNavigation" element={<CampusNavigation />} />
-          <Route path="/HomePage" element={<HomePage />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Landing />} />
+        <Route path="/BusSchedule" element={<BusSchedule />} />
+        <Route path="/CafeteriaMenu" element={<CafeteriaMenu />} />
+        <Route path="/EventCalendar" element={<EventCalendar />} />
+        <Route path="/ClassSchedule" element={<ClassSchedule />} />
+        <Route path="/CampusNavigation" element={<CampusNavigation />} />
+        <Route path="/HomePage" element={<HomePage />} />
+        
+        {/* Nested Admin Routes */}
+        <Route path="/Admin">
+          <Route index element={<AdminDashboard />} />
+          <Route path="CafeteriaManagement" element={<CafeteriaManagement />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- server/models/Meal.js
+++ server/models/Meal.js
@@ -1,23 +1,21 @@
 // const mongoose = require("mongoose");
 import mongoose from "mongoose";
+import mongooseSequence from "mongoose-sequence";
 
 const mealSchema = new mongoose.Schema({
+  mealId: { type: Number, unique: true },
   name: { type: String, required: true },
   description: { type: String, required: true },
   price: { type: Number, required: true },
-  image: { type: String, required: true },
-  nutrition: {
-    calories: { type: Number, required: true },
-    protein: { type: Number, required: true },
-    carbs: { type: Number, required: true },
-    fat: { type: Number, required: true },
-    fiber: { type: Number, required: true },
-  },
-  allergens: { type: [String], required: true },
-  categories: { type: [String], required: true },
-  available: { type: Boolean, default: true },
-  popularity: { type: Number, default: 0 },
-  prepTime: { type: String, required: true },
+  image: { type: String},
+  categories: { type: [String]},
+  available: { type: Boolean},
+  prepTime: { type: String},
+});
+
+mealSchema.plugin(mongooseSequence(mongoose), {
+  inc_field: 'mealId',  // Specify the field that will be auto-incremented
+  start_seq: 1,         // Start incrementing from 1
 });
 
 // module.exports = mongoose.model("Meal", mealSchema);
