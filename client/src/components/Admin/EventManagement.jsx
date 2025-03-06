d86145f0df990096d9c55344ee4c462ef69f1b25
Ishmaam Iftekhar Khan <ishmaam@iut-dhaka.edu>
2025-03-07 03:30:08 +0600

    Events not loading

--- client/src/components/Admin/EventManagement.jsx
+++ client/src/components/Admin/EventManagement.jsx
@@ -909,7 +909,7 @@ const EventManagement = () => {
                                                         className="block w-full px-3 py-2 border border-gray-300 dark:border-gray-600 rounded-lg bg-white dark:bg-gray-700 text-gray-900 dark:text-gray-100 focus:ring-2 focus:ring-indigo-500 focus:border-transparent"
                                                     >
                                                         <option value="attending">Attending</option>
-                                                        <option value="maybe">Maybe</option>
+                                                        <option value="invited">Invited</option>
                                                         <option value="declined">Declined</option>
                                                     </select>
                                                 </div>
